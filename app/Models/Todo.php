587fb7c70ea6674da1d42e4346441d83e67cc463
dabour1 <aldabor63@yahoo.com>
2024-12-11 22:54:16 +0200

    "Removed unused trait annotation from Todo model."

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -7,7 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Todo extends Model
 {
-    /** @use HasFactory<\Database\Factories\TodoFactory> */
     use HasFactory;
 
     protected $fillable = [
